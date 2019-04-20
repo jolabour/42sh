992e136c678503bd77d2f944cfbf37e79e181f15
Armand CHAVY <achavy@e1r13p14.42.fr>
2019-04-20 02:30:10 +0200

    norm_ok

---
+++ src/armand/ft_utils_exp_ari.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils_exp_ari.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achavy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/20 02:19:40 by achavy            #+#    #+#             */
+/*   Updated: 2019/04/20 02:28:24 by achavy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sh.h"
+
+char			*my_get_var(t_42sh *sh, char *name)
+{
+	char	*str;
+	int		i;
+	char	*ret;
+
+	i = 0;
+	ret = NULL;
+	str = ft_strjoin(name, "=");
+	if ((ret = ft_getenv(sh->env, str, ft_strlen(str), sh->var)))
+	{
+		free(str);
+		return (ft_strdup(ret));
+	}
+	free(str);
+	return (ft_strdup("0"));
+}
+
+t_list_ari		*ft_add_var_exp(char *str, size_t *i, int j, char **nb)
+{
+	t_list_ari	*new;
+
+	new = NULL;
+	new = (t_list_ari*)malloc(sizeof(t_list_ari));
+	new->var = ft_strdup(*nb);
+	new->name = ft_strsub(str, *i, j);
+	new->next = NULL;
+	new->nbr = ft_atoi(*nb);
+	new->opt = ft_var_modif(str, *i, j, new);
+	if (new->opt == 1 || new->opt == 2)
+	{
+		free(*nb);
+		*nb = NULL;
+		*nb = ft_strdup(new->var);
+	}
+	return (new);
+}
+
+void			ft_replace_var(t_list_ari *tmp, t_42sh *sh)
+{
+	char	*tmp_str;
+	char	*str;
+
+	str = NULL;
+	tmp_str = NULL;
+	tmp_str = ft_strjoin(tmp->name, "=");
+	str = ft_strjoin(tmp_str, tmp->var);
+	free(tmp_str);
+	check_local_variable(sh, str);
+	free(str);
+}
+
+void			ft_free_ari(t_list_ari *list, char *str)
+{
+	t_list_ari *tmp;
+
+	tmp = list;
+	while (tmp)
+	{
+		free(tmp->var);
+		free(tmp->name);
+		list = tmp;
+		tmp = tmp->next;
+		free(list);
+	}
+	if (str)
+		free(str);
+}
